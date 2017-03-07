f17f241715abb1fb063fdc7e9901f259bb9dd610
e5ther <estherpriebe99@gmail.com>
2017-03-07 15:51:12 -0500

    added Trudy files
    first Turtle project

---
+++ trudy.py
@@ -0,0 +1,11 @@
+import turtle
+wn = turtle.Screen
+trudy = turtle.Turtle()
+trudy.shape(“turtle”)
+trudy.color(“blue”)
+trudy.pencolor(“orange”)
+for i in range(4):
+	trudy.forward(40)
+	trudy.right(90)
+
+turtle.mainloop()
